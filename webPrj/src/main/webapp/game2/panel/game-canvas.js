f925c0c71b7f865bf9a0daf0907553d401c60c35
song <dhyn.song@gmail.com>
2023-01-06 18:21:38 +0900

    1-6

--- webPrj/src/main/webapp/game2/panel/game-canvas.js
+++ webPrj/src/main/webapp/game2/panel/game-canvas.js
@@ -1,19 +1,22 @@
-class GameCanvas{
+class GameCanvas {
 
-    constructor(){
+    constructor() {
         this.dom = document.querySelector(".game-canvas");
-    /** @type {CanvasRenderingContext2D}*/
-    this.ctx = this.dom.getContext("2d");
-    this.boy = new Boy(100, 100);
-    
-    this.gameover = false;
-    this.pause = false;
+        this.dom.focus(); // 창이 뜨지마자 키 입력 가능
+        /** @type {CanvasRenderingContext2D}*/
+        this.ctx = this.dom.getContext("2d");
+        this.boy = new Boy(100, 100);
+        this.bg = new Background();
 
-    this.dom.onclick = this.clickHandler.bind(this); //bind가 없으면 main 캔버스
+        this.gameover = false;
+        this.pause = false;
+
+        this.dom.onclick = this.clickHandler.bind(this); //bind가 없으면 main 캔버스
+        this.dom.onkeydown = this.keyDownHandler.bind(this);
 
     }
 
-    run(){
+    run() {
         //60프레임 표시
         this.update();
         this.draw();
@@ -30,33 +33,57 @@ class GameCanvas{
         //     this.run();  //this는 window
         // },3000);
 
-        window.setTimeout(()=>{ //밖 this를 쓰지만 지역화가 없음
+        window.setTimeout(() => { //밖 this를 쓰지만 지역화가 없음
             this.run();
             console.log("time out");
-        },100);
+        }, 15);
     }
-    update(){
+    update() {
         this.boy.update();
         // this.boy.move(2);
 
     }
-    draw(){
+    draw() {
+        this.bg.draw(this.ctx);
         this.boy.draw(this.ctx);
     }
 
-    pause1(){
+    pause1() {
         this.pause = true;
     }
 
     //---------event handlers------------------
-    clickHandler(e){
+    clickHandler(e) {
         // this.pause = true;
-        
+
         this.boy.moveTo(e.x, e.y);
         // this.boy.move(2);
         // this.boy.draw(this.ctx);
-    
-}
+
+    }
+    keyDownHandler(e) {
+        
+        switch (e.key) {
+            case 'ArrowUp':
+                this.boy.move(1);
+                break;
+
+            case 'ArrowRight':
+                this.boy.move(2);
+                break;
+
+            case 'ArrowDown':
+                this.boy.move(3);
+                break;
+
+            case 'ArrowLeft':
+                this.boy.move(4);
+                break;
+
+            default:
+                break;
+        }
+    }
 }
 
 
